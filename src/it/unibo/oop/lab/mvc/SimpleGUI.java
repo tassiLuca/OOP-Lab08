b0c30d1803ebf7f425a9fe4d7b225108e76edf91
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-17 22:17:22 +0100

    Made the imports explicit

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -8,7 +8,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 /**
