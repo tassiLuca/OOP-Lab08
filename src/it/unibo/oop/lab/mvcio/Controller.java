00c9c0b479b820880867feec2d4bf925d49f2d67
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-21 13:00:31 +0100

    Improved formatting and variables name

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -10,26 +10,7 @@ import java.io.IOException;
  * file at a time, and it is able to serialize objects int it.
  */
 public class Controller {
-    /*
-     * This class must implement a simple controller responsible of I/O access. It
-     * considers a single file at a time, and it is able to serialize objects in it.
-     * 
-     * Implement this class with:
-     * 
-     * 1) A method for setting a File as current file
-     * 
-     * 2) A method for getting the current File
-     * 
-     * 3) A method for getting the path (in form of String) of the current File
-     * 
-     * 4) A method that gets a String as input and saves its content on the current
-     * file. This method may throw an IOException.
-     * 
-     * 5) By default, the current file is "output.txt" inside the user home folder.
-     * A String representing the local user home folder can be accessed using
-     * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
-     * Windows) can be obtained as String through the method
-     * System.getProperty("file.separator"). The combined use of those methods leads
+    /* The combined use of those methods leads
      * to a software that runs correctly on every platform.
      */
     private static final String DEFAULT_PATH = System.getProperty("user.home") 
@@ -66,7 +47,7 @@ public class Controller {
 
     /**
      * 
-     * @return the pathname string of the current file.
+     * @return the pathname string of the current file
      */
     public String getFilePath() {
         return this.file.toString();
@@ -74,14 +55,16 @@ public class Controller {
 
     /**
      * 
-     * @param strToBeSaved
+     * @param text
+     *          the text to save
      * @throws IOException 
+     *          if the writing fails
      */
-    public void saveString(final String strToBeSaved) throws IOException {
+    public void saveString(final String text) throws IOException {
         try (BufferedWriter w = new BufferedWriter(
                 new FileWriter(this.file))
         ) {
-            w.write(strToBeSaved);
+            w.write(text);
             w.newLine();
         }
     }
