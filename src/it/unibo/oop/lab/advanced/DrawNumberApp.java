528cebbb788e8040ff3530c9ecdd4f4ef9ad19a4
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-23 21:31:38 +0100

    Removed settings constant and update consistently with the new class to read configs from file

--- src/it/unibo/oop/lab/advanced/DrawNumberApp.java
+++ src/it/unibo/oop/lab/advanced/DrawNumberApp.java
@@ -4,9 +4,6 @@ package it.unibo.oop.lab.advanced;
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
 
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
     private final DrawNumber model;
     private final DrawNumberView view;
 
@@ -14,7 +11,9 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      * 
      */
     public DrawNumberApp() {
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        Config.readGameSettings();
+        this.model = new DrawNumberImpl(Config.getMin(), 
+                Config.getMax(), Config.getAttempts());
         this.view = new DrawNumberViewImpl();
         this.view.setObserver(this);
         this.view.start();
